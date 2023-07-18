33fcab8b784f34d47ac665eb42b6e7b8c964ab36
leikerdgs <leiker.dgs@gmail.com>
2023-07-18 18:45:57 +0200

    practice strings method

---
+++ practice-js/stringsMethods/1.charAt.js
@@ -0,0 +1,17 @@
+//here, understanding charAt method
+
+const str = "Hola";
+
+console.log(str.charAt(3));
+
+//replicating the method
+const index = 3;
+const j = 0;
+
+while (j <= index)
+{
+    if (j == index)
+    {
+        console.log(str[j]);
+    }
+}
